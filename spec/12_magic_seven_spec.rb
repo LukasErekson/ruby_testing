26ac414da5e4af4de8d8912100b7494be3a264f6
Lukas Erekson <50251026+LukasErekson@users.noreply.github.com>
2021-08-27 01:01:47 +0000

    Provide solutions for exercises 1-13

--- spec/12_magic_seven_spec.rb
+++ spec/12_magic_seven_spec.rb
@@ -70,34 +70,57 @@ describe MagicSeven do
   # Write a test for each of the following methods:
 
   describe '#subtract_four' do
+    context 'when the previous step is 16' do
+      it 'returns 12' do
+        previous_step = 16
+        result = game.subtract_four(previous_step)
+        expect(result).to eq(12)
+      end
+    end
   end
 
   describe '#divide_by_two' do
+    context 'when the previous step is 12' do
+      it 'returns 6' do
+        previous_step = 12
+        result = game.divide_by_two(previous_step)
+        expect(result).to eq(6)
+      end
+    end
   end
 
   # The following tests will need you to create new instances of MagicSeven with
   # a specific value for the random_number.
   describe '#subtract_random_number' do
+    let(:my_game) { described_class.new(7) }
+    context 'when the random number is 7' do
+      it 'stores 7 as an attribute' do
+        expect(my_game.random_number).to be 7
+      end
+    end
   end
 
   # The #play method will always return seven! Test this game, using any
   # positive integer as the random_number. Update the context with the number.
   describe '#play' do
-    context 'when the random number is ...' do
+    context 'when the random number is 4' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(described_class.new(4).play).to eq 7
       end
     end
 
-    context 'when the random number is ...' do
+    context 'when the random number is 3167' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(described_class.new(3167).play).to eq 7
       end
     end
 
-    context 'when the random number is ...' do
+    context 'when the random number is 8675309' do
       # remove the 'x' before running this test
-      xit 'will return 7' do
+      it 'will return 7' do
+        expect(described_class.new(8675309).play).to eq 7
       end
     end
   end
